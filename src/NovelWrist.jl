b075ae4ed0da89503d351f1c7a4144dde9c23c90
afernandez <adriano.del_rio_fernandez@dfki.de>
2022-03-08 21:31:50 +0100

    module definition

--- src/NovelWrist.jl
+++ src/NovelWrist.jl
@@ -1,3 +1,6 @@
+module NovelWrist 
+
+# imported packages
 using LinearAlgebra
 using Plots
 using ForwardDiff
@@ -6,11 +9,16 @@ using JLD2
 using StaticArrays
 using PolygonOps
 
-# functions
-
 # Includes 
 include("geometrictypes.jl")
 include("helperfunctions.jl")
 include("kinematics.jl")
 include("plotting.jl")
 
+# kinematics
+export inverse_kinematics, Jacobian
+
+# performance analysis
+export plot_configuration_space, plot_conditioning, plot_conditioning_C, plot_singularities_C, plot_torque_C
+
+end 
