577804eb37e73d26900b13a278ec52bafc174ae5
afernandez <adriano.del_rio_fernandez@dfki.de>
2022-03-11 01:41:35 +0100

    some beauty corrections

--- test/kinematic_test.jl
+++ test/kinematic_test.jl
@@ -5,22 +5,22 @@ module kinematic_test
     ###################
     # hardcoded tests #
     ###################
-
-    build_wrist = WristGeometry(l = (0.045, 0.045), 
-                                r = (0.049, 0.049), 
-                                r_ = (0.049, 0.049),
-                                h = (0.012, 0.012),
-                                b = ([0.015, -0.178, -0.034], [-0.015, -0.178, -0.034]),
-                                c = ([0.015, -0.032, 0.011], [-0.015, -0.032, 0.011]),
-                                e0 = ([0.027, 0, -0.030], [-0.027, 0, -0.030]),
-                                n = ([1, 0, 0], [-1, 0, 0]),
-                                actuator_limits = ((0.113, 0.178), (0.113, 0.178))); 
-    x = [0,0] # end-effector in zero position
-    @test inverse_kinematics(x, build_wrist, specsol = [1,2], intrinsic = true) ≈ [0.13347357815533836, 0.13347357815533836]
-    @test Jacobian(x, build_wrist; specsol = [1,2], intrinsic = true, split = false)[1,1] ≈ 15.963257027677201
-    @test Jacobian(x, build_wrist; specsol = [1,2], intrinsic = true, split = false)[1,2] ≈ 15.963257027677201
-    @test Jacobian(x, build_wrist; specsol = [1,2], intrinsic = true, split = false)[2,1] ≈ 17.736952252974675
-    @test Jacobian(x, build_wrist; specsol = [1,2], intrinsic = true, split = false)[2,2] ≈ -17.736952252974675
-    
+    @testset " " begin
+        build_wrist = WristGeometry(l = (0.045, 0.045), 
+                                    r = (0.049, 0.049), 
+                                    r_ = (0.049, 0.049),
+                                    h = (0.012, 0.012),
+                                    b = ([0.015, -0.178, -0.034], [-0.015, -0.178, -0.034]),
+                                    c = ([0.015, -0.032, 0.011], [-0.015, -0.032, 0.011]),
+                                    e0 = ([0.027, 0, -0.030], [-0.027, 0, -0.030]),
+                                    n = ([1, 0, 0], [-1, 0, 0]),
+                                    actuator_limits = ((0.113, 0.178), (0.113, 0.178))); 
+        x = [0,0] # end-effector in zero position
+        @test inverse_kinematics(x, build_wrist, specsol = [1,2], intrinsic = true) ≈ [0.13347357815533836, 0.13347357815533836]
+        @test Jacobian(x, build_wrist; specsol = [1,2], intrinsic = true, split = false)[1,1] ≈ 15.963257027677201
+        @test Jacobian(x, build_wrist; specsol = [1,2], intrinsic = true, split = false)[1,2] ≈ 15.963257027677201
+        @test Jacobian(x, build_wrist; specsol = [1,2], intrinsic = true, split = false)[2,1] ≈ 17.736952252974675
+        @test Jacobian(x, build_wrist; specsol = [1,2], intrinsic = true, split = false)[2,2] ≈ -17.736952252974675
+    end 
 end 
 
