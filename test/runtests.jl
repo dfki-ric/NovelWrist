094420a4981ad29a1fc37b7b03bfb236c0b828a5
Christoph <christoph.stoeffler@dfki.de>
2022-03-14 16:50:58 +0100

    Corrected false tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ using Test
     for q1 in LinRange(lim[1][1], lim[1][2], 20)
         for q2 in LinRange(lim[2][1], lim[2][2], 20)
             x = NovelWrist.forward_kinematics([q1, q2], RH5_wrist, solution = [2,1,1])
-            @test inverse_kinematics(x, RH5_wrist, solution = [1,2]) ≈ q
+            @test inverse_kinematics(x, RH5_wrist, solution = [1,2]) ≈ [q1, q2]
         end
     end
 end
@@ -17,7 +17,7 @@ end
     for q1 in LinRange(lim[1][1], lim[1][2], 20)
         for q2 in LinRange(lim[2][1], lim[2][2], 20)
             x = NovelWrist.forward_kinematics([q1, q2], RH5_wrist, solution = [2,1,1], intrinsic = false)
-            @test inverse_kinematics(x, RH5_wrist, solution = [1,2], intrinsic = false) ≈ q
+            @test inverse_kinematics(x, RH5_wrist, solution = [1,2], intrinsic = false) ≈ [q1, q2]
         end
     end
 end
