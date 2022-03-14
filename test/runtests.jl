6531007d81f22b79ef9255fa329e01aab87173b5
Christoph <christoph.stoeffler@dfki.de>
2022-03-14 17:40:11 +0100

    Reducing runtests to the minimum

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Test
 # testing forward and inverse solutions inside feasible region"
 @testset "kinematic tests (intrinsic)" begin
     for q1 in LinRange(0.13, 0.145, 5)
-        for q2 in LinRange(lim[2][1], 0.145, 5)
+        for q2 in LinRange(0.13, 0.145, 5)
             x = NovelWrist.forward_kinematics([q1, q2], RH5_wrist, solution = [2,1,1])
             @test NovelWrist.inverse_kinematics(x, RH5_wrist, solution = [1,2]) ≈ [q1, q2]
         end
